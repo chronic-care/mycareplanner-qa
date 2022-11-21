8d992a79c37718496efe9762f36edf65eb24a692
David Carlson <dcarlson@clinicalcloud.solutions>
2022-11-21 16:14:37 -0700

    Updates

---
+++ precache-manifest.72c7198c62936280ba0aad21a9e6f4cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f439eaf5f43d41f8ddc5800f13390bdd",
+    "url": "/mycareplanner-qa/index.html"
+  },
+  {
+    "revision": "a29440ec968aa229dc52",
+    "url": "/mycareplanner-qa/static/css/5.3fac59f9.chunk.css"
+  },
+  {
+    "revision": "b325452253fcabc44263",
+    "url": "/mycareplanner-qa/static/css/main.5afc21ec.chunk.css"
+  },
+  {
+    "revision": "b5845bfd79a3127ae801",
+    "url": "/mycareplanner-qa/static/js/0.c72242b5.chunk.js"
+  },
+  {
+    "revision": "a29440ec968aa229dc52",
+    "url": "/mycareplanner-qa/static/js/5.0713188d.chunk.js"
+  },
+  {
+    "revision": "5cd256696a08d11f2e9445e1de25eecb",
+    "url": "/mycareplanner-qa/static/js/5.0713188d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a1886d9007914b4759",
+    "url": "/mycareplanner-qa/static/js/launch.e4232fdc.chunk.js"
+  },
+  {
+    "revision": "b325452253fcabc44263",
+    "url": "/mycareplanner-qa/static/js/main.b745675e.chunk.js"
+  },
+  {
+    "revision": "4f0a5c175331dec27a32",
+    "url": "/mycareplanner-qa/static/js/runtime-launch.22279828.js"
+  },
+  {
+    "revision": "fb5bcead27eff5dd8a2f",
+    "url": "/mycareplanner-qa/static/js/runtime-main.2a90d2ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mycareplanner/precache-manifest.dd75e56a46a0c695f0c09b7eb7901d05.js"
+  "/mycareplanner-qa/precache-manifest.72c7198c62936280ba0aad21a9e6f4cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mycareplanner/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mycareplanner-qa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
